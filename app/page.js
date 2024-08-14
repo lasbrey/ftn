80edf63de4fd3575eb46d3e618d42d4844c65e68
lasbrey <lazarusosilawal.com>
2024-08-14 12:28:57 +0100

    update

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 "use client";
-import Layout from "../../components/layout/Layout";
-import About2 from "../../components/sections/About2";
-import Banner2 from "../../components/sections/Banner2";
-import Coinlist2 from "../../components/sections/Coinlist2";
-import Crypto1 from "../../components/sections/Crypto1";
-import Services1 from "../../components/sections/Services1";
-import Work1 from "../../components/sections/Work1";
-import Invest from "../../components/investhome";
+import Layout from "../components/layout/Layout";
+import About2 from "../components/sections/About2";
+import Banner2 from "../components/sections/Banner2";
+import Coinlist2 from "../components/sections/Coinlist2";
+import Crypto1 from "../components/sections/Crypto1";
+import Services1 from "../components/sections/Services1";
+import Work1 from "../components/sections/Work1";
+import Invest from "../components/investhome";
 export default function Home2() {
   return (
     <>
